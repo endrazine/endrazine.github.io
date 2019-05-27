50a1a16aa02c237a56e3aa0044de2db71ac36297
Jonathan Brossard <endrazine@gmail.com>
2019-05-27 01:58:15 -0700

    Initial commit.

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var cacheVersion = 5;
+var currentCache = {
+  offline: 'offline-cache' + cacheVersion
+};
+const offlineUrl = 'index.html';
+
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache.offline).then(function(cache) {
+      return cache.addAll([
+	offlineUrl,
+	'/IoT/security/metrics/logo-white.png'
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', event => {
+  // request.mode = navigate isn't supported in all browsers
+  // so include a check for Accept: text/html header.
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+          fetch(event.request.url).catch(error => {
+              // Return the offline page
+              return caches.match(offlineUrl);
+          })
+    );
+  }
+  else{
+        // Respond with everything else if we can
+        event.respondWith(caches.match(event.request)
+                        .then(function (response) {
+                        return response || fetch(event.request);
+                    })
+            );
+      }
+});
+
